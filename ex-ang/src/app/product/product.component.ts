93afdf33545246814346f8c70a35b412800133e4
dthiep3012 <49056826+taetae301296@users.noreply.github.com>
2021-01-03 15:39:46 +0700

    truyen params tuy chon

--- ex-ang/src/app/product/product.component.ts
+++ ex-ang/src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ShopService } from 'src/app/services/shop.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -8,14 +9,27 @@ import { ShopService } from 'src/app/services/shop.service';
 })
 export class ProductComponent implements OnInit {
   products: any;
-  constructor(private shopService: ShopService) { }
+  pages: number[];
+  currentPage: number;
+  filter: string;
+
+  constructor(
+    private shopService: ShopService,
+    private _activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this._activatedRoute.queryParams.subscribe(params => {
+      this.currentPage = params['pageNumber'] || 1;
+      this.filter = params['filter'] || '';
+    });
+
     this.shopService.getList().subscribe(res => {
       return this.products = res;
     }, error => {
       console.log(error);
     });
+    this.pages = [1, 2, 3, 4, 5];
   }
 
 }
